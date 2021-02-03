ffee4232abc3eb68af8272261672aecf82b6a0b4
Tracy Golden <tracy80s2003@gmail.com>
2021-02-02 19:23:08 -0600

    added html routes

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -8,3 +8,23 @@ let notesInfo = [];
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
+
+
+
+
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
+});
+  
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+});
+  
+app.get("/api/notes", function(req, res) {
+    return res.sendFile(path.json(__dirname, "Develop/db/db.json"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`)
+});
